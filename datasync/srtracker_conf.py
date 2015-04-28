065a0fa31379405fdbdf9dbc46723857da5f3b49
Victor Lu <lzp401@gmail.com>
2015-04-28 17:56:31 +0800

    Remove some critical informations

--- datasync/srtracker_conf.py
+++ datasync/srtracker_conf.py
@@ -6,11 +6,6 @@ import os
 sql_file_path = os.path.dirname(os.path.realpath(__file__)) + '/sqls/%s'
 
 pg_config = {
-    'database': 'sr_tracker_db',
-    'user': 'sr_tracker_sa',
-    'password': 'ca$hc0w',
-    # 'host': '127.0.0.1',
-    'host': '10.111.100.113',
     'temp_table': 'source_data',
     'sql': (
         sql_file_path % 'pg_init.sql',
@@ -20,11 +15,6 @@ pg_config = {
 }
 
 oracl_config = {
-    'host': 'PRDAP-DWDB.ENG.VMWARE.COM',
-    'sid': 'PRDAPD',
-    'user': 'sa_da_user',
-    'password': 'P8Sdvq1$se2',
-    'port': 1521,
     'sql': sql_file_path % 'oracl_query_source.sql',
 }
 
